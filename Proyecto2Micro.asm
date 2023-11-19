b43663615737a21413f30c9cba01b4329d251ecb
WalterVillatoro1204 <wevillatorom@correo.url.edu.gt>
2023-11-19 16:45:47 -0600

    arreglando los errores

--- Proyecto2Micro.asm
+++ Proyecto2Micro.asm
@@ -17,6 +17,12 @@
 
 # Macro para llenar la primera matriz
 #------------------------------------------MACRO-----------------------------------------
+
+.macro sacarvalores (%filas1, %columna1, %filas12, %columna12)
+
+
+.end_macro 
+
 .macro llenarMatriz (%filas, %columnas)
  
  	     li s10, 0
@@ -48,18 +54,12 @@
             add s9, a1, s8  # Dirección en la matriz
 
             # Almacenar el valor en la matriz
-            sb t6, 0(s9)
-            
-            
-            
-            mv a0, t6
-            
+            sb t6, 0(s9) 
+            mv a0, t6   
             addi a0, a0, 48
             
            li a7, 11
            ecall 
-           
-
             # Incrementar índice de columna
             addi s10, s10, 1
 
@@ -96,7 +96,6 @@ programa:
     ecall
     mv t1, a0
 
-   
     # Obtener el decimal de cantMatrices
     addi t1, t1, -48
 
@@ -107,16 +106,13 @@ programa:
     # Limitar cantidad de matrices = 3 matrices
     li s1, 9  # Si la cantidad de matrices es igual o mayor a 4 se cierra el programa
     bge t1, s1, finalizar  # 4 matrices maximo
+    
 
 #etiqueta matrices siguientes
 MatrizFilCol:	
 
     addi t1, t1, -1  
-
-    #contador para guardar filas y columnas 
-    #contador empieza en 1 con el addi
-    li s5, 0
-    addi s5, s5 1    
+    
     #comparaciones con contador
     li s6, 1
     li s7, 2 
@@ -137,7 +133,7 @@ MatrizFilCol:
     li a7, 12  # Espera input del teclado, y la guarda en a0
     ecall
     mv t5, a0  # t1 FILAS
-    
+    mv tp, a0
     addi t5, t5, -48
     
     li a7, 11
@@ -152,14 +148,15 @@ MatrizFilCol:
     li a7, 12  # Espera input del teclado, y la guarda en a0
     ecall
     mv t2, a0  # t2 COLUMNAS
-    
+    mv ra, a0
     addi t2, t2, -48
     
     li a7, 11
     li a0, '\n'
     ecall 		
     
-    #saltos para llenar la variable con los datos de las filas y columnas		
+    #saltos para llenar la variable con los datos de las filas y columnas
+    addi s5, s5, 1		
     
     beq s5, s6, datosMatriz1
     beq s5, s7, datosMatriz2
@@ -171,43 +168,42 @@ MatrizFilCol:
     beq s5, a5, datosMatriz8
     beq s5, a6, datosMatriz9
     		
-    datosMatriz1:
-    	sb t5, 0(t6)
-    	sb t2, 1(t6)	
-    	li a7, 11
-    	ecall
+    datosMatriz1:	    
+   	sb tp, 0(t6)
+    	sb ra, 1(t6)	
+  	ecall    	
     	j LlenadoMatriz   	
     datosMatriz2:
-   	sb t5, 2(t6)
-    	sb t2, 3(t6)
+   	sb tp, 2(t6)
+    	sb ra, 3(t6)
     	j LlenadoMatriz
     datosMatriz3:
-    	sb t5, 4(t6)
-    	sb t2, 5(t6)	
+    	sb tp, 4(t6)
+    	sb ra, 5(t6)	
     	j LlenadoMatriz
     datosMatriz4:
-   	sb t5, 6(t6)
-    	sb t2, 7(t6)
+   	sb tp, 6(t6)
+    	sb ra, 7(t6)
     	j LlenadoMatriz
     datosMatriz5:
-    	sb t5, 8(t6)
-    	sb t2, 9(t6)
+    	sb tp, 8(t6)
+    	sb ra, 9(t6)
     	j LlenadoMatriz	
     datosMatriz6:
-   	sb t5, 10(t6)
-    	sb t2, 11(t6)
+   	sb tp, 10(t6)
+    	sb ra, 11(t6)
     	j LlenadoMatriz
     datosMatriz7:
-    	sb t5, 12(t6)
-    	sb t2, 13(t6)	
+    	sb tp, 12(t6)
+    	sb ra, 13(t6)	
     	j LlenadoMatriz
     datosMatriz8:
-   	sb t5, 14(t6)
-    	sb t2, 15(t6)
+   	sb tp, 14(t6)
+    	sb ra, 15(t6)
     	j LlenadoMatriz
     datosMatriz9:
-   	sb t5, 16(t6)
-    	sb t2, 17(t6)
+   	sb tp, 16(t6)
+    	sb ra, 17(t6)
     	j LlenadoMatriz
 	
     LlenadoMatriz:
@@ -218,14 +214,7 @@ MatrizFilCol:
 
     bne t1, zero, MatrizFilCol
     #J SALTOSIGUIENTEINSTRUCCION
-    #HACER UN SALTO A LO SIGUIENTE QUE SE NECESITE 
-    
-    #etiquetas para llenar la variable
-    
-    
-    
-    
-	
+
 finalizar:
     li a7, 10
     ecall
